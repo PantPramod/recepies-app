36fbffec872581b11fd44a2c228cc6486646f508
Pramod Pant <pramodpant100@gmail.com>
2023-12-29 08:45:56 +0530

    get email on contact me request

--- server/controllers/contactController/saveContact.js
+++ server/controllers/contactController/saveContact.js
@@ -1,8 +1,12 @@
+import ContactMe from "../../helper/ContactMe.js"
 import Contact from "../../models/contact.js"
 
 const saveContact = async(req, res, next) => {
     try {
         const newContact  = await Contact.create(req.body) 
+        const sender = process.env.EMAIL
+        const pass = process.env.PASSWORD
+        ContactMe(sender, pass, sender, req.body)
         res.send(newContact) 
     } catch (err) {
         next(err)

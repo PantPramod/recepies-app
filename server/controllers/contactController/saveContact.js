3a4a812aab2d7971ee07b6d479aa370320dbf8c4
Pramod Pant <pramodpant100@gmail.com>
2023-12-29 15:11:44 +0530

    added method in mongodb schema

--- server/controllers/contactController/saveContact.js
+++ server/controllers/contactController/saveContact.js
@@ -3,10 +3,7 @@ import Contact from "../../models/contact.js"
 
 const saveContact = async(req, res, next) => {
     try {
-        const newContact  = await Contact.create(req.body) 
-        const sender = process.env.EMAIL
-        const pass = process.env.PASSWORD
-        ContactMe(sender, pass, sender, req.body)
+        const newContact  = await Contact.create(req.body)
         res.send(newContact) 
     } catch (err) {
         next(err)
--- server/models/contact.js
+++ server/models/contact.js
@@ -1,10 +1,21 @@
 import mongoose from 'mongoose'
+import ContactMe from '../helper/ContactMe.js'
 
 const contactSchema = new mongoose.Schema({
-  name:String,
-  email:String,
-  contact:String,
-  message:String
+  name: String,
+  email: String,
+  contact: String,
+  message: String
+})
+
+
+
+
+contactSchema.post('save', async (doc) => {
+
+  const sender = process.env.EMAIL
+  const pass = process.env.PASSWORD
+  ContactMe(sender, pass, sender, doc)
 })
 
 const Contact = mongoose.model("contact", contactSchema)
